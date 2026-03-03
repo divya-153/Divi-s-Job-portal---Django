e14f751dcb36dc234b430c4e5942a26335d899f2
divya-153 <edivya153@gmail.com>
2026-03-03 22:26:47 +0530

    Added PostgreSQL support

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path("index/",views.index, name="index"),
+    path("",views.index, name="index"),
     path("signup/", views.signup, name="signup"),
     path("login/", views.login_view, name="login"),
     
